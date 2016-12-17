9327b68043a3a813282d88972f4670f989c3e7cf
scott-pickthorn <scott.e.pickthorn@gmail.com>
2016-12-17 14:51:38 -0800

    adding google map marker

--- public/js/main.js
+++ public/js/main.js
@@ -5,16 +5,16 @@ estimate.controller("estimateCtlr", ['$scope', '$http', function($scope, $http){
     $scope.mapscript = '';
 	profile = {}
 	$scope.getInfo = function(){
-      //   profile = {"name": $scope.name,
-					 //  "address": $scope.address,
-      //                 "cityState": $scope.cityState,
-					 //  "number": $scope.number
-					 // };
-        profile = {"name": "scott",
-                      "address": "9380 sw meadow ln",
-                      "cityState": "portland, or",
-                      "number": "503409393"
-                     };        
+        profile = {"name": $scope.name,
+					  "address": $scope.address,
+                      "cityState": $scope.cityState,
+					  "number": $scope.number
+					 };
+        // profile = {"name": "scott",
+        //               "address": "9380 sw meadow ln",
+        //               "cityState": "portland, or",
+        //               "number": "503409393"
+        //              };        
         $http.get('/house_estimate/' + profile.address + '&' + profile.cityState).then(function(response) {
             $scope.house_estimate = response.data;
             console.log($scope.house_estimate);
@@ -24,17 +24,21 @@ estimate.controller("estimateCtlr", ['$scope', '$http', function($scope, $http){
         $http.post('/info', profile).then(function(response) {
             console.log(response);
         });
+
     setTimeout(function(){
         $("#map").show();
-            map.setCenter(new google.maps.LatLng($scope.house_estimate.lat,$scope.house_estimate.lng));
-            google.maps.event.trigger(map,"resize");
-    }, 1000);
-    setTimeout(function(){
-        $("#map").show();
-            map.setCenter(new google.maps.LatLng($scope.house_estimate.lat,$scope.house_estimate.lng));
-            google.maps.event.trigger(map,"resize");
-    }, 1000);
+        latlng = new google.maps.LatLng($scope.house_estimate.lat,$scope.house_estimate.lng);
+        marker = new google.maps.Marker({
+            position: latlng,
+            title:"your house",
+            size: new google.maps.Size(171, 171),
+        });
+        marker.setMap(map);
+        google.maps.event.trigger(map,"resize");
+        map.setCenter(marker.getPosition());
+           }, 2000);
     $("#userinfo").hide();
+    $("#map-estimator").show();
     };
 
 }]);
